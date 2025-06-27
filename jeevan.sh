72cc04cc2b0fcf66b57e9a762d0001589b4ff3a7
root <root@ip-172-31-7-126.ap-southeast-2.compute.internal>
2025-06-27 13:47:36 +0000

    script file has added

---
+++ jeevan.sh
@@ -0,0 +1,7 @@
+ git config user.name "jeevankumar1019"
+ git config user.email "jeevankumarpadala01@gmail.com"
+ git config alias.s "status"
+ git config alias.lg "log --oneline --graph --all --decorate"
+ git config --list
+ git remote add origin https://github.com/JeevanKumar1019/task.git
+
